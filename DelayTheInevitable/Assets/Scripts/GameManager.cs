5905e242006468e669831e910092af2e18929983
Jonas <jonas.erkert@t-online.de>
2022-04-03 17:51:29 +0200

    Added timer and difficulty variable to game manager. Created 2D UI in main scene to display timer.

--- DelayTheInevitable/Assets/Scripts/GameManager.cs
+++ DelayTheInevitable/Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     #region singleton stuff
+
     private static GameManager _instance;
 
-    public static GameManager Instance { get { return _instance; } }
+    public static GameManager Instance
+    {
+        get { return _instance; }
+    }
 
 
     private void Awake()
@@ -21,19 +26,64 @@ public class GameManager : MonoBehaviour
             _instance = this;
         }
     }
+
     #endregion
 
     public bool gameScreenOpen = true;
+
+    [SerializeField] private float secondsToMaxDifficulty = 180f;
+    [SerializeField] private TextMeshProUGUI timerText;
+    private TimeSpan _timePlaying;
+    private bool _timerRunning = false;
+    private float _elapsedTime = 0f;
+    private float _difficulty = 0;
+
+    public float Difficulty
+    {
+        get => _difficulty;
+        set => _difficulty = Mathf.Clamp(value, 0, 1);
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this);
+        
+        BeginTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void BeginTimer()
+    {
+        _timerRunning = true;
+        _elapsedTime = 0f;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void EndTimer()
+    {
+        _timerRunning = false;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (_timerRunning)
+        {
+            _elapsedTime += Time.deltaTime;
+            Difficulty = _elapsedTime / secondsToMaxDifficulty;
+            
+            _timePlaying = TimeSpan.FromSeconds(_elapsedTime);
+            string timePlayingString = _timePlaying.ToString("mm':'ss':'ff");
+            timerText.text = timePlayingString;
+
+            yield return null;
+        }
     }
 }
