6eec2497aafd480679832e123c506aa514612629
DErmark <dome.ermark@gmail.com>
2022-04-04 18:18:49 +0200

    Desktop Game simple tutorial for keys

---
+++ DelayTheInevitable/Assets/Scripts/DesktopGameTutorial.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopGameTutorial : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject,3.0f);
+    }
+ 
+    
+}
